45ea0cb3b6947eec159d99c808f46835f90d9cb0
shahnewaz601 <shahnewaz601@gmail.com>
2021-01-19 06:02:05 +0600

    simple update

--- Fibonacci.js
+++ Fibonacci.js
@@ -14,3 +14,12 @@ console.log(fibo);
 
 
 //===Using Function====//
+function fabonacchi(n){
+    var fibo = [0, 1];
+    for(var i = 2; i <= n; i++){
+        fibo[i] = fibo[i-1] + fibo[i-2];
+    }
+    return fibo;
+}
+var result = fabonacchi(12);
+console.log(result);
